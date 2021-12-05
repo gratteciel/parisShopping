c5eb93c3bbf0440263b1b810ee7f075698e1ed8c
Mathis Buet <mbuetelfassy@gmail.com>
2021-12-05 22:56:56 +0100

    Ajout : bootstrap

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <!-- Include du CSS Bootstrap -->
+        <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php include("header.php"); ?>
+        <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
+
+
+        
