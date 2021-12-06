9878d0333e5c3181dfc5c9d07a5aeb5e59f364a8
gratteciel <emilian.mitu@edu.ece.fr>
2021-12-06 13:28:12 +0100

    Finalisation de la map et Ajout du footer

--- src/footer.php
+++ src/footer.php
@@ -1,1 +1,30 @@
-<?php
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+        <div class="col-12 col-md">
+            <img class="mb-2" src="../assets/brand/bootstrap-logo.svg" alt="" width="24" height="19">
+            <small class="d-block mb-3 text-muted">&copy; 2020-2021</small>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Nous contacter</h5>
+            <ul class="list-unstyled text-small">
+                <li class="mb-1">Mail : <a class="link-secondary text-decoration-none" href="mailto:parisShopping@yahoo.com">parisShopping@yahoo.com</a></li>
+                <li class="mb-1">Téléphone : <a class="link-secondary text-decoration-none" href="tel:02-41-37-65-24<">02 41 37 65 24</a></li>
+                <li class="mb-1">Adresse : 37 Quai de Grenelle, 75015 Paris</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Notre projet</h5>
+            <ul class="list-unstyled text-small">
+                <li class="mb-1"><a class="link-secondary text-decoration-none" href="equipe.html">L'Equipe</a></li>
+                <li class="mb-1"><a class="link-secondary text-decoration-none" href="site.html">Le Site</a></li>
+                <li class="mb-1"><a class="link-secondary text-decoration-none" href="cahier_des_charges.html">Cahier des charges</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>A propos</h5>
+            <ul class="list-unstyled text-small">
+                <li class="mb-1"><a class="link-secondary text-decoration-none" href="mentions.html">Mentions légales</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
