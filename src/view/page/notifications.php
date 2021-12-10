0ecb3d40ff3695c72d5c2021ae39b2dd27b8d48b
Vincent Jouanneau <63442366+VincentJouanneau@users.noreply.github.com>
2021-12-10 14:17:41 +0100

    Notification finis

--- src/view/page/notifications.php
+++ src/view/page/notifications.php
@@ -1,50 +1,24 @@
 <?php
-$notificationList = [
-(object) [
-'id'      => 1,
-'name'    => 'Inscription',
-'time'   => 12,
-'des' => 'Merci pour vous être inscrit',
-],
-(object) [
-'id'      => 2,
-'name'    => 'Verification de mail',
-'time'   => 11,
-'des' => 'Votre mail a bien été verifié',
-    ],
-(object) [
-'id'      => 3,
-'name'    => 'Vente accepté',
-'time'   => 8,
-'des' => 'Merci pour la moula',
-],
-(object) [
-'id'      => 4,
-'name'    => 'Annulation de commande',
-'time'   => 6,
-'des' => 'Votre commande a été annulé',
-],
-(object) [
-'id'      => 5,
-'name'    => 'Carte graphique',
-'time'   => 3,
-'des' => 'Jean-Louis a accepté le prix que vous avez négocié',
-],
-];
+
+$notificationList =  requeteSqlArray("SELECT * from notification where idUtilisateur= '{$_SESSION['idUtilisateur']}'ORDER BY `date` DESC", $pdo)
+
 
 //include count($notificationList) ? 'view/notification/list.php' : '../product/noProducts.php';
 ?>
 
-<div class="notification">
+<div>
 <?php foreach ($notificationList as $notificationInfo): ?>
-        <a href="#" class="list-group-item list-group-item-action">
+    <div class="list-group-item list-group-item-action>
+
+        <a href="index.php?page=article&id=<?php echo $notificationInfo['idArticle']?>">
             <div class="d-flex w-100 justify-content-between">
-                <h5 class="mb-1"><?php echo $notificationInfo->name; ?></h5>
-                <small> il y a <?php echo $notificationInfo->time; ?> jour</small>
+                <h5 class="mb-1"><?php echo $notificationInfo['nom']; ?></h5>
+                <small> <?php echo $notificationInfo['date']; ?> </small>
             </div>
-            <p class="mb-1"><?php echo $notificationInfo->des; ?></p>
-            <small>And some small print.</small>
-            </a>
+            <p class="mb-1"><?php echo $notificationInfo['description']; ?></p>
+        </a>
+        <small><button onclick="location.href='script_php/Notification/suppNotification.php?idNotification=<?php echo $notificationInfo['idNotification'] ?>'" type="button" class="btn btn-danger">Supprimer la notification</button></small>
+    </div>
 
 <?php endforeach; ?>
 </div>
