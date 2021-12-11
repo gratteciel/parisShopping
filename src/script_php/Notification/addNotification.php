28bf936168ab9bb25f0a48ef264e3dda8dcbc2f3
Vincent Jouanneau <63442366+VincentJouanneau@users.noreply.github.com>
2021-12-11 12:46:32 +0100

    Mail

--- src/script_php/Notification/addNotification.php
+++ src/script_php/Notification/addNotification.php
@@ -1,4 +1,13 @@
 <?php
+
+    $header="MIME-Version: 1.0\r\n";
+    $header.='From:"ParisShopping"<parisshopingece@gmail.com>'."\n";
+    $header.='Content-Type:text/html; charset="uft-8"'."\n";
+    $header.='Content-Transfer-Encoding: 8bit';
+
+
+
+
     include('../../bdd/donneeSession.php');
     include('../../bdd/connectBDD.php');
     date_default_timezone_set('Europe/Paris');
@@ -16,12 +25,35 @@
         $nom = "Rupture";
         $description = $_REQUEST['nom']." est en rupture de stock";
     }
+    //pour structurer un mail
+
+$message='<html>
+        <body>
+            <div align="center">
+                '.$description.' 
+                
+            </div>
+        </body>
+    </html>
+    ';
+
+    //
 
     $alertList = requeteSqlArray("SELECT * from alertestock Where idArticle = '{$_REQUEST['idArticle']}' ", $pdo);
     foreach ($alertList as $alertInfo){
         requeteSqlArray("INSERT INTO notification (nom,descriptionNotif,dateNotif,idUtilisateur,lien) VALUES ('{$nom}','{$description}','{$date}', '{$alertInfo['idUtilisateur']}','article&id={$_REQUEST['idArticle']}');",$pdo);
 
+        //pour envoyer un mail
+        $utilisateur = requeteSqlArray("SELECT * from utilisateur where idUtilisateur = '{$alertInfo['idUtilisateur']}'", $pdo)
+        mail($utilisateur[0]['mail'],$message,);
+        //
+
     }
+
+
+
+
+
 
 header('Location: ../../index.php?page=article&id='.$_REQUEST["idArticle"]);
 
